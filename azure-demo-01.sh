3da8c19ea00440a8aff81015a04b1de2fd543b7e
bobbylim <bobby.limitra@gmail.com>
2018-12-01 18:34:50 +0700

    10

--- azure-demo-01.sh
+++ azure-demo-01.sh
@@ -1,4 +1,10 @@
 
+# sudo su
+# change directory to ansible (cd /R31/github/ansible)
+# Make sure to login to azure (az login)
+# Delete known host file (nano /root/.ssh/known_hosts)
+# run shell script (./azure-demo-01.sh)
+
 START_TIME=$SECONDS
 
 ansible-playbook -i inventory/hosts playbook/azure/pb-azure-vn.yaml
