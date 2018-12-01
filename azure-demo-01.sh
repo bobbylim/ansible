407a322f656db2a298e0e891c91da94de9c4ec7b
bobbylim <bobby.limitra@gmail.com>
2018-12-01 18:30:24 +0700

    9

--- azure-demo-01.sh
+++ azure-demo-01.sh
@@ -7,5 +7,5 @@ cat command/azure-demo-01.txt | parallel "{}"
 ELAPSED_TIME=$(($SECONDS - $START_TIME))
 
 echo " "
-echo " TotaL Duration:"
+echo "TotaL Duration:"
 echo "$(($ELAPSED_TIME/60)) min $(($ELAPSED_TIME%60)) sec"
--- test.sh
+++ test.sh
@@ -6,5 +6,5 @@ sleep 10
 ELAPSED_TIME=$(($SECONDS - $START_TIME))
 
 echo " "
-echo " TotaL Duration:"
+echo "TotaL Duration:"
 echo "$(($ELAPSED_TIME/60)) min $(($ELAPSED_TIME%60)) sec"
