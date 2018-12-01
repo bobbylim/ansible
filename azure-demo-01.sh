ef06ba2607afd5efae5249f90543dc8620ed11cf
bobbylim <bobby.limitra@gmail.com>
2018-12-01 18:17:59 +0700

    4

---
+++ azure-demo-01.sh
@@ -0,0 +1,11 @@
+
+START_TIME=$SECONDS
+
+ansible-playbook -i inventory/hosts playbook/azure/pb-azure-vn.yaml
+cat azure-demo-01.txt | parallel "{}"
+
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+
+echo " "
+echo " TotaL Duration:"
+echo "$(($ELAPSED_TIME/60)) min $(($ELAPSED_TIME%60)) sec"
