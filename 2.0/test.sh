06cc6057cef1b9b69ae1d27999040c7f7fd25e77
“bobby” <bobby.limitra@gmail.com>
2019-03-24 06:19:32 -0700

    23

--- 2.0/test.sh
+++ 2.0/test.sh
@@ -1,12 +1,1 @@
-
-START_TIME=$SECONDS
-
-sleep 10
-
-ELAPSED_TIME=$(($SECONDS - $START_TIME))
-
-echo " "
-echo "TotaL Duration:"
-echo "$(($ELAPSED_TIME/60)) min $(($ELAPSED_TIME%60)) sec"
-echo " "
-echo " "
+ANSIBLE 2.0
