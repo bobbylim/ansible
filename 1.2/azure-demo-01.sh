3aa59dc583a751216f6f44f5de59f5419ab6be27
“bobby” <bobby.limitra@gmail.com>
2019-04-05 08:27:58 -0700

    21

--- 1.2/azure-demo-01.sh
+++ 1.2/azure-demo-01.sh
@@ -1,10 +1,6 @@
 # This script will automatically Create F5-BIGIP and UBUNTU VM on Azure
 
-# sudo su
-# change directory to ansible (cd /R31/github/ansible)
 # make sure to login to azure (az login)
-# delete known host file (nano /root/.ssh/known_hosts)
-# input F5 BIGIP license on ansible playbook (pb-azure-vm-bigip-01.yaml)
 # run shell script (./azure-demo-01.sh)
 
 START_TIME=$SECONDS
